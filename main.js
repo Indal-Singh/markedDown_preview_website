69a6d4fdeaa104700fa289ddd83fba4f911bbc7d
Indal Singh <ind9546@gmail.com>
2025-03-24 23:25:21 +0530

    final submit

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const editor = document.getElementById('editor');
 const preview = document.getElementById('preview');
+const resetBtn = document.getElementById('resetBtn');
 
 editor.addEventListener('input', (e) => {
     let input = e.currentTarget.value;
     preview.style.display = input ? 'block' : 'none';
     preview.innerHTML = marked.parse(input);
+})
+
+resetBtn.addEventListener('click', () => {
+    editor.value = '';
+    preview.style.display = 'none';
 })
